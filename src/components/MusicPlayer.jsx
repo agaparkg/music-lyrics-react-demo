21ea0fea7c7fdb2fe80210c2481bd5ef8e91bbce
Azamat Gaparov <agaparovkg@gmail.com>
2024-05-08 19:04:17 -0700

    new changes

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-const MusicPlayer = () => {
-  const src =
-    "http://cdn-preview-c.deezer.com/stream/c-cca63b2c92773d54e61c5b4d17695bd2-8.mp3";
+const MusicPlayer = ({ src }) => {
   return (
     <div className="music-player">
       <audio controls>
--- src/components/SingleSong.jsx
+++ src/components/SingleSong.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Button } from "reactstrap";
 import MusicPlayer from "./MusicPlayer";
 
-const SingleSong = () => {
+const SingleSong = ({ song }) => {
+  const {
+    artist: { name },
+    title,
+    preview,
+  } = song;
+
   return (
     <li>
-      <span>{/* <strong>{name}</strong> - {title} */}</span>
+      <span>
+        <strong>{name}</strong> - {title}
+      </span>
       <div className="lyrics-player">
-        {/* <Button color="success">Get Lyrics</Button>
-        <MusicPlayer src={preview} /> */}
+        <Button color="success">Get Lyrics</Button>
+        <MusicPlayer src={preview} />
       </div>
     </li>
   );
